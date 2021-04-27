624185b5c0305fd2fd080660f202df8be4b8063b
DarkFuzzyCookie <david-kagan@hotmail.ca>
2021-04-27 04:59:01 -0400

    new flags added
    New options added for checking whether or not the player is swimming / falling / inside a bulding

--- client/config.lua
+++ client/config.lua
@@ -1,10 +1,13 @@
 ---------------------------------------------------------- GROUND PLAYER BY HQLY DEVELOPMENT ---------------------------------------------
 config = {
 	['displayText'] = '~y~Press ~g~E~s~ ~y~to teleport to ground!', -- message which will be displayed
-	['key'] = 38, 													-- key which activates the teleport
+	['key'] = 38, 													-- key which activates the teleport (https://docs.fivem.net/docs/game-references/controls/)
 	['preset'] = false,												-- false: place ped at current location on ground | true: place ped at preset location in config
-	['coords'] = { x=0.0, y=0.0, z=0.0 },									-- coordinates for preset location
+	['coords'] = { x=0.0, y=0.0, z=0.0 },							-- coordinates for preset location
 	['z_check'] = 0.0,												-- z co-ordinate to prompt the player at, found 0.0 to be the best
-	['freeze'] = true,												-- whether or not to freeze the player for specified amount after teleporting
-	['freeze_time'] = 2												-- number of seconds to freeze player
+	['freeze'] = false,												-- whether or not to freeze the player for specified amount after teleporting
+	['freeze_time'] = 2,											-- number of seconds to freeze player
+	['check_swimming'] = true,										-- ignore players that are swimming
+	['check_falling'] = true,										-- ignore players that are not falling
+	['check_inside'] = true,										-- ignore players that are inside buildings - experimental -
 }
