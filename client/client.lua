4609fc00fd76b34d5e0510c872d047155e024a65
DarkFuzzyCookie <david-kagan@hotmail.ca>
2021-04-27 04:58:18 -0400

    comments + new flag checks
    added comments + flag checks to see if the ped is swimming and / or falling and / or inside a building. Although I'm not too sure how the inside the building checks work as I was not able to test it.

--- client/client.lua
+++ client/client.lua
@@ -3,37 +3,58 @@ Citizen.CreateThread(function()
 	Citizen.Wait(1000)
 	
 	while true do
-		local _ped = PlayerPedId()
-		local _coords = GetEntityCoords(_ped)
+		-- get the details
+		_ped = PlayerPedId()
+		-- check if player is dead or not existing
+		if IsEntityDead(_ped) or not DoesEntityExist(_ped) then
+			Citizen.Wait(1000) -- delay it a bit to optimize a little bit
+			goto m_next
+		end
 		
+		_coords = GetEntityCoords(_ped)
+		_, z = GetGroundZFor_3dCoord(_coords.x, _coords.y, 150.0, 0) -- starting at 150.0 at Z since it works up-down, but not down-up	
+		-- is ped lower than the z co-ordinate we want to look at
 		if _coords.z < config.z_check then
-			drawTxt(config.displayText, 0, 1, 0.5, 0.8, 0.4, 128, 128, 128, 255)
+			-- flag checking per config file for: ped is swimming/falling/inside
+			local flag_swimming, flag_falling, flag_inside = true, true, true
+			if config.check_swimming and (IsPedSwimming(_ped) or IsPedSwimmingUnderWater(_ped)) then
+				flag_swimming = false
+			end
+			if config.check_falling and not IsPedFalling(_ped) then
+				flag_falling = false
+			end
+			if config.check_inside and not IsPedFalling(_ped) and z > _coords.z then 
+				flag_inside = false
+			end
 			
-			if IsControlJustReleased(0, config.key) then
-				gotcords, place = GetGroundZFor_3dCoord(_coords.x, _coords.y, 150.0, 0)
-	
+			-- display the prompt for trigger
+			if flag_falling and flag_swimming and flag_inside then
+				drawTxt(config.displayText, 0, 1, 0.5, 0.8, 0.4, 128, 128, 128, 255)
+			end
+			if IsControlJustReleased(0, config.key) and (flag_falling and flag_swimming and flag_inside) then
 				ClearPedTasksImmediately(_ped)
+				-- whether to teleport the player to the ground or predefined location
 				if config.preset then
 					SetEntityCoords(_ped, config.coords.x, config.coords.y, config.coords.z, true, false, false, false)
 				else
-					SetEntityCoords(_ped, _coords.x, _coords.y, place + 1.0, true, false, false, false)
+					SetEntityCoords(_ped, _coords.x, _coords.y, z + 1.0, true, false, false, false) -- +1.0 on Z to compensate for _ped height
 				end
-				
+				-- freeze the player for x seconds per config file
 				if config.freeze then
 					Citizen.CreateThread(function()
 						FreezeEntityPosition(_ped, true)
-						
 						local timer = config.freeze_time
 						while timer > 0 do
 							timer = timer - 1
 							Citizen.Wait(1000)
 						end
-						
 						FreezeEntityPosition(_ped, false)
 					end)
 				end
 			end
 		end
+		
+		::m_next::
 		Citizen.Wait(5)
 	end
 end)
